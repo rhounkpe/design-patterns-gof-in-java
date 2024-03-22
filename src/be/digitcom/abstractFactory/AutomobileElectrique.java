25475870b84e9345a204b430179355e7e3e5271d
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2024-03-22 10:29:53 +0100

    Automobile Electrique

---
+++ src/be/digitcom/abstractFactory/AutomobileElectrique.java
@@ -0,0 +1,16 @@
+package be.digitcom.abstractFactory;
+
+public class AutomobileElectrique extends Automobile {
+    public AutomobileElectrique(String modele, String couleur, int puissance, double espace) {
+        super(modele, couleur, puissance, espace);
+    }
+
+    @Override
+    public void afficheCaracteristiques() {
+        System.out.println(
+                "Automobile électrique de modele: " + modele +
+                        " de couleur: " + couleur + " de puissance " +
+                        puissance + " d'espace: " +  espace
+        );
+    }
+}
